6ff42aa63bbb1540b976053567d065199bb01034
Téo Asseline <asselineteo@gmail.com>
2024-02-09 14:47:45 +0100

    clean project

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+    }
+}
