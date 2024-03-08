f9c61c2c9441e0eebcff12bca1f4102afb60394e
Téo Asseline <asselineteo@gmail.com>
2024-03-08 11:00:59 +0100

    Refactoring / Amélioration du code

---
+++ src/main/java/FonctionMath.java
@@ -0,0 +1,14 @@
+public class FonctionMath {
+    public static void factoriel() {
+        // Throw non implemented exception
+    }
+    public static void puissance() {
+        // Throw non implemented exception
+    }
+
+    public static void main(String[] args) {
+        //Test de nos fonctions de calculs
+        factoriel();
+        //puissance();
+    }
+}
--- src/main/java/RenforcerLaSecurite.java
+++ src/main/java/RenforcerLaSecurite.java
@@ -1,19 +1,10 @@
 
 public class RenforcerLaSecurite {
-    public static void factoriel() {
-        // Throw non implemented exception
-    }
-    public static void puissance() {
-        // Throw non implemented exception
-    }
     public static void connexion(){
         // Throw non implemented exception
     }
     public static void main(String[] args) {
-        //Test de nos fonctions de calculs
-        factoriel();
-        //puissance();
         //Test de notre fonction de connexion
-        //connexion();
+        connexion();
     }
 }
